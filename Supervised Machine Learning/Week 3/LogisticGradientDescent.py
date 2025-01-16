9f09c6974eb343148b6780f7b024c413081fd9dd
Hami Ibriyamov <hamop3108@gmail.com>
2025-01-16 16:07:45 +0200

    Create LogisticGradientDescent.py

---
+++ Supervised Machine Learning/Week 3/LogisticGradientDescent.py
@@ -0,0 +1,93 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# Dataset
+X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_train = np.array([0, 0, 0, 1, 1, 1])
+
+# SIGMOID FUNCTION
+def sigmoid(z):
+
+    g = 1/(1+np.exp(-z))
+   
+    return g
+
+#LOGISTIC COST FUNCTION
+def compute_cost_logistic(X, y, w, b):
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)
+             
+    cost = cost / m
+    return cost
+
+# GRADIENT DESCENT
+def compute_gradient_logistic(X, y, w, b): 
+    m,n = X.shape
+    dj_dw = np.zeros((n,))                           #(n,)
+    dj_db = 0.
+
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i],w) + b)          #(n,)(n,)=scalar
+        err_i  = f_wb_i  - y[i]                       #scalar
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i,j]      #scalar
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw/m                                   #(n,)
+    dj_db = dj_db/m                                   #scalar
+        
+    return dj_db, dj_dw  
+
+# Check implementation
+X_tmp = X_train
+y_tmp = y_train
+w_tmp = np.array([2.,3.])
+b_tmp = 1.
+dj_db_tmp, dj_dw_tmp = compute_gradient_logistic(X_tmp, y_tmp, w_tmp, b_tmp)
+print(f"dj_db: {dj_db_tmp}" )
+print(f"dj_dw: {dj_dw_tmp.tolist()}" )
+
+#Expected output
+# dj_db: 0.49861806546328574
+# dj_dw: [0.498333393278696, 0.49883942983996693]
+
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters): 
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+      
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( compute_cost_logistic(X, y, w, b) )
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
+        
+    return w, b, J_history         #return final w,b and J history for graphing
+
+
+# RUN GRADIENT DESCENT 
+w_tmp  = np.zeros_like(X_train[0])
+b_tmp  = 0.
+alph = 0.1
+iters = 10000
+
+w_out, b_out, _ = gradient_descent(X_train, y_train, w_tmp, b_tmp, alph, iters) 
+print(f"\nupdated parameters: w:{w_out}, b:{b_out}") # Last values will be the best parameters because the cost is the lowest
+
