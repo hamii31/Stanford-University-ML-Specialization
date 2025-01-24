0349bfe0e58dcaf1623b32f3087b617cef45f0c0
Hami Ibriyamov <hamop3108@gmail.com>
2025-01-24 12:59:59 +0200

    Create NeuronWithoutActivation.py

---
+++ Advanced Learning Algorithms/Week 1/NeuronWithoutActivation.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras import Sequential
+from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
+from tensorflow.keras.activations import sigmoid
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+# Dataset
+X_train = np.array([[1.0], [2.0]], dtype=np.float32)           #(size in 1000 square feet)
+Y_train = np.array([[300.0], [500.0]], dtype=np.float32)       #(price in 1000s of dollars)
+
+fig, ax = plt.subplots(1,1)
+ax.scatter(X_train, Y_train, marker='x', c='r', label="Data Points")
+ax.legend( fontsize='xx-large')
+ax.set_ylabel('Price (in 1000s of dollars)', fontsize='xx-large')
+ax.set_xlabel('Size (1000 sqft)', fontsize='xx-large')
+plt.show()
+
+# Linear Layer
+linear_layer = tf.keras.layers.Dense(units=1, activation = 'linear', )
+
+linear_layer.get_weights()
+
+# Tensor (Array) with shape 1,1
+a1 = linear_layer(X_train[0].reshape(1,1))
+print(a1)
+
+# Weights with random values
+w, b= linear_layer.get_weights()
+print(f"w = {w}, b={b}")
+
+set_w = np.array([[200]])
+set_b = np.array([100])
+
+# set_weights takes a list of numpy arrays
+linear_layer.set_weights([set_w, set_b])
+print(linear_layer.get_weights())
+
+# Let's compare equation (1) to the layer output.
+a1 = linear_layer(X_train[0].reshape(1,1))
+print(a1)
+alin = np.dot(set_w,X_train[0].reshape(1,1)) + set_b
+print(alin)
