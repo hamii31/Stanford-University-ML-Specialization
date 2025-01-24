331d8ef1a7052a4e98af121d956500dd37798daf
Hami Ibriyamov <hamop3108@gmail.com>
2025-01-24 16:34:04 +0200

    Create SimpleNeuralNetwork.py

---
+++ Advanced Learning Algorithms/Week 1/SimpleNeuralNetwork.py
@@ -0,0 +1,130 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+# A simple neural network
+
+def load_coffee_data():
+    """ Creates a coffee roasting data set.
+        roasting duration: 12-15 minutes is best
+        temperature range: 175-260C is best
+    """
+    rng = np.random.default_rng(2)
+    X = rng.random(400).reshape(-1,2)
+    X[:,1] = X[:,1] * 4 + 11.5          # 12-15 min is best
+    X[:,0] = X[:,0] * (285-150) + 150  # 350-500 F (175-260 C) is best
+    Y = np.zeros(len(X))
+    
+    i=0
+    for t,d in X:
+        y = -3/(260-175)*t + 21
+        if (t > 175 and t < 260 and d > 12 and d < 15 and d<=y ):
+            Y[i] = 1
+        else:
+            Y[i] = 0
+        i += 1
+
+    return (X, Y.reshape(-1,1))
+
+X,Y = load_coffee_data();
+print(X.shape, Y.shape)
+
+# Normalization of the data using Keras "Normalization Layer"
+print(f"Temperature Max, Min pre normalization: {np.max(X[:,0]):0.2f}, {np.min(X[:,0]):0.2f}")
+print(f"Duration    Max, Min pre normalization: {np.max(X[:,1]):0.2f}, {np.min(X[:,1]):0.2f}")
+norm_l = tf.keras.layers.Normalization(axis=-1)
+norm_l.adapt(X)  # learns mean, variance
+Xn = norm_l(X)
+print(f"Temperature Max, Min post normalization: {np.max(Xn[:,0]):0.2f}, {np.min(Xn[:,0]):0.2f}")
+print(f"Duration    Max, Min post normalization: {np.max(Xn[:,1]):0.2f}, {np.min(Xn[:,1]):0.2f}")
+
+
+# Tile/copy our data to increase the training set size and reduce the number of training epochs.
+Xt = np.tile(Xn,(1000,1))
+Yt= np.tile(Y,(1000,1))   
+print(Xt.shape, Yt.shape) 
+
+# Tensorflow Network with Sigmoid Activations
+
+tf.random.set_seed(1234)  # applied to achieve consistent results
+model = Sequential(
+    [
+        tf.keras.Input(shape=(2,)),
+        Dense(3, activation='sigmoid', name = 'layer1'),
+        Dense(1, activation='sigmoid', name = 'layer2')
+     ]
+)
+
+model.summary()
+
+L1_num_params = 2 * 3 + 3   # W1 parameters  + b1 parameters
+L2_num_params = 3 * 1 + 1   # W2 parameters  + b2 parameters
+print("L1 params = ", L1_num_params, ", L2 params = ", L2_num_params  )
+
+# Let's examine the weights and biases Tensorflow has instantiated. The weights 𝑊 should be of size (number of features in input, number of units in the layer) while the bias 𝑏
+
+# size should match the number of units in the layer:
+
+#     In the first layer with 3 units, we expect W to have a size of (2,3) and 𝑏
+
+# should have 3 elements.
+# In the second layer with 1 unit, we expect W to have a size of (3,1) and 𝑏
+# should have 1 element.
+
+
+W1, b1 = model.get_layer("layer1").get_weights()
+W2, b2 = model.get_layer("layer2").get_weights()
+print(f"W1{W1.shape}:\n", W1, f"\nb1{b1.shape}:", b1)
+print(f"W2{W2.shape}:\n", W2, f"\nb2{b2.shape}:", b2)
+
+
+model.compile(
+    loss = tf.keras.losses.BinaryCrossentropy(),
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01),
+)
+
+model.fit(
+    Xt,Yt,            
+    epochs=10,
+)
+
+# Epochs and batches
+
+# In the fit statement above, the number of epochs was set to 10. This specifies that the entire data set should be applied during training 10 times. During training, you see output describing the progress of training that looks like this:
+
+# Epoch 1/10
+# 6250/6250 [==============================] - 6s 910us/step - loss: 0.1782
+
+# The first line, Epoch 1/10, describes which epoch the model is currently running. For efficiency, the training data set is broken into 'batches'. The default size of a batch in Tensorflow is 32. There are 200000 examples in our expanded data set or 6250 batches. The notation on the 2nd line 6250/6250 [==== is describing which batch has been executed.
+
+# Updated weights after fitting
+W1, b1 = model.get_layer("layer1").get_weights()
+W2, b2 = model.get_layer("layer2").get_weights()
+print("W1:\n", W1, "\nb1:", b1)
+print("W2:\n", W2, "\nb2:", b2)
+
+# Testing
+X_test = np.array([
+    [200,13.9],  # positive example
+    [200,17]])   # negative example
+X_testn = norm_l(X_test)
+predictions = model.predict(X_testn)
+print("predictions = \n", predictions)
+
+# Treshhold
+yhat = np.zeros_like(predictions)
+for i in range(len(predictions)):
+    if predictions[i] >= 0.5:
+        yhat[i] = 1
+    else:
+        yhat[i] = 0
+print(f"decisions = \n{yhat}")
+
+# A better version
+yhat = (predictions >= 0.5).astype(int)
+print(f"decisions = \n{yhat}")
