81a5cfbb9799b0cf558692d698830deb34f6b8d9
Hami Ibriyamov <hamop3108@gmail.com>
2025-02-27 13:04:12 +0200

    Create State-action value func.py

---
+++ Unsupervised Learning, Recommenders, Reinforcement Learning/Week 3/State-action value func.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Do not modify
+num_states = 6
+num_actions = 2
+
+terminal_left_reward = 100
+terminal_right_reward = 40
+each_step_reward = 0
+
+# Discount factor
+gamma = 0.5
+
+# Probability of going in the wrong direction
+misstep_prob = 0
